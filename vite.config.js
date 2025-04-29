e59f9fa815991139b8391c5df3728c71410fd6c9
arfath-ali <1290sfaa28@gmail.com>
2025-04-29 17:43:07 +0530

    update files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/planets-facts-site',
+  base: './',
   plugins: [react(), tailwindcss()],
 });
