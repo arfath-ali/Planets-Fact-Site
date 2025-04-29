20156380d9d7da20843130f661ccf5a8c2148dfc
arfath-ali <1290sfaa28@gmail.com>
2025-04-29 18:55:22 +0530

    update files

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: './',
   plugins: [react(), tailwindcss()],
+  server: {
+    historyApiFallback: true, 
+  },
+  build: {
+    outDir: 'dist',
+  }
 });
