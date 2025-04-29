e4441fa6db32a24b80bb53daaf85ce6ca31c3274
arfath-ali <1290sfaa28@gmail.com>
2025-04-29 13:52:22 +0530

    update file

--- src/components/PlanetsFactDashboard.jsx
+++ src/components/PlanetsFactDashboard.jsx
@@ -3,6 +3,7 @@ import PlanetsMobileNav from './mobile/PlanetsMobileNav.jsx';
 import PlanetsNav from './PlanetsNav.jsx';
 import PlanetsRoutes from './PlanetsRoutes.jsx';
 import MenuIcon from '../assets/images/icon-hamburger.svg';
+import Earth from '../pages/earth.jsx';
 
 const PlanetsFactDashboard = () => {
   const [toogleMenu, setToogleMenu] = useState(false);
@@ -12,7 +13,7 @@ const PlanetsFactDashboard = () => {
 
   return (
     <>
-      <header className="px-6 py-4 md:pt-8 md:pb-[1.6875rem] md:px-[3.25rem] lg:pl-8 lg:pr-10">
+      <header className="px-6 py-4 md:px-[3.25rem] md:pt-8 md:pb-[1.6875rem] lg:pr-10 lg:pl-8">
         <div className="flex items-center justify-between md:hidden">
           <h1 className="font-antonio font-weight-medium leading-md text-[1.75rem] tracking-tight">
             THE PLANETS
@@ -24,7 +25,7 @@ const PlanetsFactDashboard = () => {
           />
         </div>
 
-        <div className="hidden flex-col items-center gap-10 md:flex lg:flex-row lg:gap-0 lg:justify-between">
+        <div className="hidden flex-col items-center gap-10 md:flex lg:flex-row lg:justify-between lg:gap-0">
           <h1 className="font-antonio font-weight-medium leading-md text-[1.75rem] tracking-tight">
             THE PLANETS
           </h1>
@@ -35,6 +36,7 @@ const PlanetsFactDashboard = () => {
       <main>
         <div className="bg-dark-gray h-px w-full"></div>
         {toogleMenu && <PlanetsMobileNav hideMenu={handleMenuClick} />}
+        <Earth />
         {!toogleMenu && <PlanetsRoutes />}
       </main>
 
